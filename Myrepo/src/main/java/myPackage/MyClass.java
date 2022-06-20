533373c30e8b4b0da83e7cc800ed6c5f3b743d94
Shubhangi Askar <105338597+AskarShubhangi@users.noreply.github.com>
2022-06-20 15:22:07 +0530

    commit2
    commited by shubhangi

---
+++ Myrepo/src/main/java/myPackage/MyClass.java
@@ -0,0 +1,16 @@
+package myPackage;
+
+public class MyClass {
+	
+	public void abc()
+	{
+		System.out.println("abc");
+	}
+	
+	public static void main(String args[])
+	{
+		MyClass abc =new MyClass();
+		abc.abc();
+	}
+
+}
